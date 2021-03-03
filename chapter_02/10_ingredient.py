65c4d5be02114c5cd7b1cb4892f1ffcaf1472d7c
SergeHall <sergioartg@gmail.com>
2021-03-03 01:20:45 -0800

    Chapter 03

--- chapter_02/10_ingredient.py
+++ chapter_02/10_ingredient.py
@@ -1,11 +1,10 @@
-
 # Ingredient Adjuster
 
 # Ask user how many cookies he or she wants
-cookie=int(input("Enter the amount of cookies you want: "))
+cookie = int(input("Enter the amount of cookies you want: "))
 sugar = 1.5
 oil = 1
 pounder = 2.75
-print(sugar * cookie, ' сахара.\n',
-      oil * cookie, ' масла.\n',
-      pounder * cookie, ' муки.', sep="")
+print(sugar * cookie, ' gr. sugar.\n',
+      oil * cookie, ' gr. butter.\n',
+      pounder * cookie, ' gr. flour.', sep="")
