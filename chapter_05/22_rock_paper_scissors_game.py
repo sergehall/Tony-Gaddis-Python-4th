6d28b19d6dd17949d9635782c0e243f9e07fc5c0
SergeHall <sergioartg@gmail.com>
2021-03-04 02:06:59 -0800

    Chapter 05

--- chapter_05/22_rock_paper_scissors_game.py
+++ chapter_05/22_rock_paper_scissors_game.py
@@ -18,11 +18,6 @@ def main():
 def scissors_rock_paper(player, comp):
     if player == comp:
         print('Draw, try again', 'comp_players', '=', comp, '\n')
-        # player = int(input('Entered: 1 if rock, '
-        #                           '2 if scissors, '
-        #                           '3 if paper.\n'))
-        # comp_players = random.randint(1, 3)
-        # scissors_rock_paper(first_players, comp_players)
     elif player == 1 and comp == 2:
         print('You Win!', 'computer', '=', comp)
     elif player == 2 and comp == 3:
